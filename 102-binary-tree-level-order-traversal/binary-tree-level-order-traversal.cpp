54f95d8228d964f984cdecbc24c01b9145986217
Sumit Yadav <sumityadav73397@gmail.com>
2026-08-23 17:13:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (16.06%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -32,11 +32,11 @@ public:
                 level.push_back(node->val);
             
             
-                if(node->left!=nullptr){
+                if(node->left){
                     q.push(node->left);
                 }
 
-                if(node->right!=nullptr){
+                if(node->right){
                     q.push(node->right);
                 }
 
