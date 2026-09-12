408e0a4a6f1e8eb954f8e82821be16681263c0f7
Sumit Yadav <sumityadav73397@gmail.com>
2026-09-12 23:26:27 +0530

    Time: 11 ms (34.81%) | Memory: 12.4 MB (56.43%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -10,14 +10,14 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
 
-        ListNode * prev=NULL;
-        while(node != NULL && node->next!=NULL){
-            node->val=node->next->val;
-            prev=node;
-            node=node->next;
-        }
-        prev->next=NULL;
-        delete(node);
+       ListNode * prev=NULL;                          // first make a node name prev
+       while(node!=NULL && node->next!=NULL){         // check if node is null or the next of the node is null
+        node->val=node->next->val;                    // then put the valuse of the next node to the current node
+        prev=node;                                    // now point the prev node to the current node
+        node=node->next;
+       }
+       prev->next=NULL;                               //once we get the condition executed then make prev next val to be null
+       delete(node);                                  // now delete the node
         
     }
 };
